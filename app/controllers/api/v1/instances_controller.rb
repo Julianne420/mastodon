c0789305e1a00189edc5e5a372c8764a7c0629b3
Julianne420 <you@example.com>
2021-06-09 13:34:38 +0800

    Hide /api/v1/instance API in secure mode

--- app/controllers/api/v1/instances_controller.rb
+++ app/controllers/api/v1/instances_controller.rb
@@ -2,7 +2,6 @@
 
 class Api::V1::InstancesController < Api::BaseController
   skip_before_action :set_cache_headers
-  skip_before_action :require_authenticated_user!, unless: :whitelist_mode?
 
   def show
     expires_in 3.minutes, public: true
